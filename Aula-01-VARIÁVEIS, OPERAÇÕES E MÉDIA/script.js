b2eca0387ffc9661b483061ba49e516bc44df261
Willian da silva soares <90284411+willianglitch@users.noreply.github.com>
2022-03-13 10:51:15 -0300

    class-01
    progress

---
+++ Aula-01-VARIÁVEIS, OPERAÇÕES E MÉDIA/script.js
@@ -0,0 +1,26 @@
+
+function calcularMédia(){
+  // trazendo os valores para dentro da função
+  const nota01 = parseFloat(document.getElementById("nota1").value);
+  const nota02 = parseFloat(document.getElementById("nota2").value);
+  const nota03 = parseFloat(document.getElementById("nota3").value);
+  const nota04 = parseFloat(document.getElementById("nota4").value);
+  
+  const pegandoResultado = document.getElementById("resultado")
+  
+  // fazendo o calculor da média
+  const calculandoMédia = (nota01 + nota02 + nota03 + nota04 ) /4
+  
+  console.log(calculandoMédia)
+  
+  if (calculandoMédia >= 7 ){
+    pegandoResultado.innerHTML = " Você passou parabéns " ;
+  }
+  else if (calculandoMédia > 10 || calculandoMédia < 0 ){
+    pegandoResultado.innerHTML =  " coloque um número decimal entre 0 a 10 " ;
+  }
+  else{
+    pegandoResultado.innerHTML = " tente novamente no próximo semestre " ;
+  }
+  
+}
